1b2f29ada713bbcdbfc5298d7f345c444a0a8d02
Olesia Vovk <olesia_ivanenko@epam.com>
2018-07-26 20:22:11 +0000

    Enable sse_algorithm AES256

--- main.tf
+++ main.tf
@@ -13,6 +13,14 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
     prevent_destroy = true
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = "${merge(var.tags)}"
 }
 
